54431c70eeb7232e4ac93a1256a7e525f43f2e6c
Zach Hart <zachhart.eng@gmail.com>
2025-08-13 04:07:21 +0100

    Got initial matrix library working

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,11 @@
 #include <iostream>
 
 #include <math_utilities/vector.hpp>
+#include <math_utilities/matrix2D.hpp>
 
 template <typename T> T pi(3.1415926535);
 
 int main() {
-  std::cout << pi<int> << std::endl;
-  std::cout << pi<float> << std::endl;
-  std::cout << pi<double> << std::endl;
-
   Vector<int, 3> test_vec = {1, 2, 3};
 
   for (std::size_t i = 0; i < 3; i++) {
@@ -34,6 +31,27 @@ int main() {
   for (std::size_t i = 0; i < 3; i++) {
     std::cout << output[i] << '\n'; 
   }
+
+  // for (int i = 0; i < 1000000; i++) {
+  //   if (i % 1000 == 0) {
+  //       std::cout << i << '\n';
+  //   }
+  // }
+
+  Matrix2D<int, 2, 3> matrix = {1, 2, 3, 4, 5, 6};
+  matrix.print();
+
+  Matrix2D<int, 3, 2> matrix_2 = {1, 2, 3, 4, 5, 6};
+  matrix_2.print();
+
+  std::cout << matrix(1,5) << "\n";
+
+  Matrix2D<int, 2, 2> new_matrix = matrix * matrix_2;
+  new_matrix.print(); 
+
+  // Matrix2D<int, 2, 2> new_matrix_2 = matrix_2 * matrix;
+  // new_matrix.print(); 
+
 
   return 0;
 }
--- src/math_utilities/matrix2D.hpp
+++ src/math_utilities/matrix2D.hpp
@@ -3,13 +3,77 @@
 
 #include <iostream>
 
-class Matrix2D {
-    public:
-        template <typename T> T matrix; 
+template <typename T, std::size_t rows, std::size_t columns>
+class Matrix2D 
+{
+public:
+    static_assert(std::is_arithmetic_v<T>, "Matrix2D requires numeric types.");
+    std::array<std::array<T, columns>, rows> matrix;
+    
+    Matrix2D() = default; 
+
+    Matrix2D(std::initializer_list<T> initial_matrix) {
+        if (rows * columns != initial_matrix.size()) {
+            throw std::out_of_range("Size of matrix does not equal size of initializer list");
+        }   
+        
+        std::size_t row    = 0; 
+        std::size_t column = 0;
+        for (const T& element : initial_matrix) {
+            matrix[row][column] = element; 
+            column = column + 1; 
+            if (column == columns) {
+                column = 0; 
+                row = row + 1; 
+            }
+        }
+    }
+
+    void print() const {
+        for (std::size_t row = 0; row < rows; row++) {
+            for (std::size_t column = 0; column < columns; column++) {
+                std::cout << matrix[row][column] << "   "; 
+            }
+            std::cout << "\n";
+        }
+    }
+
+    T& operator()(std::size_t desired_row, std::size_t desired_column) {
+        return matrix[desired_row][desired_column];
+    }
+
+    const T& operator()(std::size_t desired_row, std::size_t desired_column) const {
+        return matrix[desired_row][desired_column];
+    }
+
+    template <std::size_t other_columns>
+    Matrix2D<T, rows, other_columns> operator*(const Matrix2D<T, columns, other_columns>& right_hand_side) const {
+       
+        const Matrix2D<T, rows, columns>& left_hand_side = *this; 
+        Matrix2D<T, rows, other_columns> new_matrix{}; 
+        
+        for (std::size_t new_row = 0; new_row < rows; new_row++) {
+            for (std::size_t new_column = 0; new_column < other_columns; new_column++) {
+                for (std::size_t shared_dimension = 0; shared_dimension < columns; shared_dimension++) {
+                    new_matrix(new_row, new_column) += left_hand_side(new_row, shared_dimension) * right_hand_side(shared_dimension, new_column);
+                }
+            }
+        }
+        
+        return new_matrix; 
+    }
+
+    
+    
+    
 
 
-    private:
-}
+
+
+private:
+
+
+};
 
 
 #endif
