7ef72d2793981c6fcac11e3e00c1e1db94b6aa47
Zachary Hart <zachhart.eng@gmail.com>
2025-10-26 21:28:50 -0500

    Adding main

--- src/main.cpp
+++ src/main.cpp
@@ -20,90 +20,94 @@
 #include "../examples/attitude_filter/apps/logging_sim_app.hpp"
 
 int main() {
-  // const std::string &sim_config_path      = "examples/attitude_filter/config_files/simulation_config.yaml";
-  // const std::string &logger_config_path   = "examples/attitude_filter/config_files/logging_config.yaml";
-  // const std::string &priority_config_path = "examples/attitude_filter/config_files/app_priorities.yaml";
-  // const std::string &gyro_config_path     = "examples/attitude_filter/config_files/gyro_config.yaml";
+  const std::string &sim_config_path      = "examples/attitude_filter/config_files/simulation_config.yaml";
+  const std::string &logger_config_path   = "examples/attitude_filter/config_files/logging_config.yaml";
+  const std::string &priority_config_path = "examples/attitude_filter/config_files/app_priorities.yaml";
+  const std::string &gyro_config_path     = "examples/attitude_filter/config_files/gyro_config.yaml";
 
-  // YAML::Node app_priority_data  = YAML::LoadFile(priority_config_path);
-  // YAML::Node sim_config_data    = YAML::LoadFile(sim_config_path);
-  // YAML::Node logger_config_data = YAML::LoadFile(logger_config_path);
-  // YAML::Node gyro_config_data   = YAML::LoadFile(gyro_config_path);
+  YAML::Node app_priority_data  = YAML::LoadFile(priority_config_path);
+  YAML::Node sim_config_data    = YAML::LoadFile(sim_config_path);
+  YAML::Node logger_config_data = YAML::LoadFile(logger_config_path);
+  YAML::Node gyro_config_data   = YAML::LoadFile(gyro_config_path);
 
-  // DataBus data_bus;
-  // Simulation sim(sim_config_path, data_bus);
+  DataBus data_bus;
+  Simulation sim(sim_config_path, data_bus);
 
-  // double gyro_rate  = gyro_config_data["rate_hz"].as<double>();
-  // int gyro_priority = app_priority_data["gyro_priority"].as<int>();
-  // std::shared_ptr<GyroSimInterface> gyro_interface = std::make_shared<GyroSimInterface>();
-  // std::shared_ptr<SimApp> gyro_app = std::make_shared<SimApp>(gyro_interface, gyro_rate, gyro_priority, gyro_config_path);
-  // sim.add_app(gyro_app);
+  double gyro_rate  = gyro_config_data["rate_hz"].as<double>();
+  int gyro_priority = app_priority_data["gyro_priority"].as<int>();
+  std::shared_ptr<GyroSimInterface> gyro_interface = std::make_shared<GyroSimInterface>();
+  std::shared_ptr<SimApp> gyro_app = std::make_shared<SimApp>(gyro_interface, gyro_rate, gyro_priority, gyro_config_path);
+  sim.add_app(gyro_app);
 
-  // std::shared_ptr<LoggingSimApp> logging_app = std::make_shared<LoggingSimApp>(logger_config_path);
-  // sim.add_logger(logging_app);
+  std::shared_ptr<LoggingSimApp> logging_app = std::make_shared<LoggingSimApp>(logger_config_path);
+  sim.add_logger(logging_app);
 
-  // sim.run();
+  sim.run();
 
-  HDF5Logger logger("zach_test.hdf5", "C:/git/personal_project/src/");
-  logger.create_file();
+  // HDF5Logger logger("zach_test.hdf5", "C:/git/personal_project/src/");
 
-  logger.add_group("group_root/group_a/group_b");
-  logger.add_group("group_root/group_c/group_d");
-  logger.add_group("group_root/group_c/group_e");
-  logger.add_group("root_group_2/group_f/group_g");
 
-  logger.add_dataset<double>("test_data_name", {2, 3, 4}, "group_root");
-  logger.add_sim_dataset<double>("sim_data_test", {2, 3, 4}, "root_group_2");
-  logger.add_sim_dataset<double>("sim_data_test", {2, 3, 4}, "group_root/group_a/group_b");
-  logger.print_file_tree();
 
-  double data_to_log = 10.0;
-  std::unique_ptr<double[]> data_buffer_in(new double[1]);
-  data_buffer_in.get()[0] = data_to_log;
+  // HDF5Logger logger("zach_test.hdf5", "C:/git/personal_project/src/");
+  // logger.create_file();
 
-  logger.write_data<double>("group_root/test_data_name", data_buffer_in.get(), {0, 0, 0}, {1, 1, 1});
+  // logger.add_group("group_root/group_a/group_b");
+  // logger.add_group("group_root/group_c/group_d");
+  // logger.add_group("group_root/group_c/group_e");
+  // logger.add_group("root_group_2/group_f/group_g");
 
-  std::unique_ptr<double[]> data_buffer_out(new double[24]);
+  // logger.add_dataset<double>("test_data_name", {2, 3, 4}, "group_root");
+  // logger.add_sim_dataset<double>("sim_data_test", {2, 3, 4}, "root_group_2");
+  // logger.add_sim_dataset<double>("sim_data_test", {2, 3, 4}, "group_root/group_a/group_b");
+  // logger.print_file_tree();
 
-  logger.read_data<double>("group_root/test_data_name", data_buffer_out.get(), {0, 0, 0}, {2, 3, 4});
+  // double data_to_log = 10.0;
+  // std::unique_ptr<double[]> data_buffer_in(new double[1]);
+  // data_buffer_in.get()[0] = data_to_log;
 
-  for (size_t i = 0; i < 24; i++) {
-      std::cout << data_buffer_out[i] << " ";
-  }
-  std::cout << std::endl;
+  // logger.write_data<double>("group_root/test_data_name", data_buffer_in.get(), {0, 0, 0}, {1, 1, 1});
+
+  // std::unique_ptr<double[]> data_buffer_out(new double[24]);
+
+  // logger.read_data<double>("group_root/test_data_name", data_buffer_out.get(), {0, 0, 0}, {2, 3, 4});
+
+  // for (size_t i = 0; i < 24; i++) {
+  //     std::cout << data_buffer_out[i] << " ";
+  // }
+  // std::cout << std::endl;
+
+  // // logger.close_file();
+
+
+  // // APPEND DATA TEST
+  // // logger.open_file();
+
+  // double sim_step_data[2 * 3 * 4] = {
+  //   0.0, 0.1, 0.2, 0.3,
+  //   1.0, 1.1, 1.2, 1.3,
+  //   2.0, 2.1, 2.2, 2.3,
+
+  //   3.0, 3.1, 3.2, 3.3,
+  //   4.0, 4.1, 4.2, 4.3,
+  //   5.0, 5.1, 5.2, 5.3
+  // };
+
+
+
+  // logger.append_data<double>("root_group_2/sim_data_test", {1, 2, 3, 4}, sim_step_data);
+  // logger.read_data<double>("root_group_2/sim_data_test", data_buffer_out.get(), {0, 0, 0, 0}, {1, 2, 3, 4});
+
+
+  // for (size_t i = 0; i < 24; i++) {
+  //     std::cout << data_buffer_out[i] << " ";
+  // }
+  // std::cout << std::endl;
 
   // logger.close_file();
 
+  // // // for (std::size_t i = 0; i < 5; i++ ) {
 
-  // APPEND DATA TEST
-  // logger.open_file();
-
-  double sim_step_data[2 * 3 * 4] = {
-    0.0, 0.1, 0.2, 0.3,
-    1.0, 1.1, 1.2, 1.3,
-    2.0, 2.1, 2.2, 2.3,
-
-    3.0, 3.1, 3.2, 3.3,
-    4.0, 4.1, 4.2, 4.3,
-    5.0, 5.1, 5.2, 5.3
-  };
-
-
-
-  logger.append_data<double>("root_group_2/sim_data_test", {1, 2, 3, 4}, sim_step_data);
-  logger.read_data<double>("root_group_2/sim_data_test", data_buffer_out.get(), {0, 0, 0, 0}, {1, 2, 3, 4});
-
-
-  for (size_t i = 0; i < 24; i++) {
-      std::cout << data_buffer_out[i] << " ";
-  }
-  std::cout << std::endl;
-
-  logger.close_file();
-
-  // // for (std::size_t i = 0; i < 5; i++ ) {
-
-  // // }
+  // // // }
 
   return 0;
 };
